936913ac64ed2984db7328a1f55085b7ce3d5cce
Thai Nguyen <thai.nguyen@s3corp.com.vn>
2018-06-15 16:31:06 +0700

    test deployment

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ var app = express();
 var path = require('path');
 
 app.use(express.static(__dirname + '/dist'));
-app.listen(4200);
 
 app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
+
+app.listen(process.env.PORT || 4200);
